22709e7acef18d52f436e95922de3ede06bb4458
DerspaB <jun-beto155@hotmail.com>
2023-04-01 23:31:13 +0200

    excel

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,6 +3,7 @@ import StudentsList from "../components/StudentsList";
 import axios from "axios";
 import { useState } from "react";
 import { Button } from "@mui/material";
+import { CSVLink } from "react-csv";
 
 export const AdminPage = () => {
   const [students, setStudents] = useState([]);
@@ -30,9 +31,18 @@ export const AdminPage = () => {
   return (
     <div className="flex flex-col items-center gap-7 w-full">
       <StudentsList students={students} />
-      <Button onClick={deleteCalendar} variant="contained" color="error">
-        Eliminar Calendario
-      </Button>
+      <div className="flex flex-wrap w-full items-center justify-center gap-5">
+        <Button onClick={deleteCalendar} variant="contained" color="error">
+          Eliminar Calendario
+        </Button>
+        <CSVLink
+          data={students}
+          filename="students"
+          className="text-white py-2 px-4 bg-green-700 rounded-md text-[15px] shadow-md"
+        >
+          EXPORTAR
+        </CSVLink>
+      </div>
     </div>
   );
 };
